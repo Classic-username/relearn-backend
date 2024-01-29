fa8172151315521482726fbb92229ac910c212a4
Classic-username <josh.drapergame@gmail.com>
2024-01-29 10:42:36 -0700

    Renamed "htm" file to "html" because I was physically cringing at the sight of "htm" Added html form for file upload, added logic to process and upload files in server.js

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var fs = require("fs");
+var multer = require("multer");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+// This is how the project wanted us to implement multer, but it isn't working when I run the server file. ChatGPT has provided a fix
+// app.use(multer({ dest: "/tmp" }));
+
+// ChatGPT implementation fix:
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "/tmp/");
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);
+    }
+});
+
+var upload = multer({ storage: storage });
+
+// end ChatGPT implementation fix
 
 // Delving into things I am struggling to comprehend
-app.use("/index.htm", (req, res) => {
-    res.sendFile(__dirname + "/" + "index.htm");
+app.use("/index.html", (req, res) => {
+    res.sendFile(__dirname + "/" + "index.html");
 });
 
 app.get("/process_get", (req, res) => {
@@ -62,6 +81,38 @@ app.get("/ab*cd", (req, res) =>  {
     console.log("Got a GET request for /ab*cd")
     res.send("Page Pattern Match");
 });
+
+// Writing logic for file uploads
+app.get("/index.html", (req, res) => {
+    res.sendFile( __dirname + '/' + "index.html");
+});
+
+// ChatGPT added another parameter in the app.post below, being the "upload.single("file")"
+app.post("/file_upload", upload.single("file"), (req, res) => {
+    console.log(req.file.originalname);
+    console.log(req.file.path);
+    console.log(req.file.mimetype);
+
+    var file = __dirname + '/' + req.file.originalname;
+
+    fs.readFile( req.file.path, (err, data) => {
+        fs.writeFile(file, data, (err) => {
+            if( err ) {
+                console.log ( err );
+                res.status(500).send("Inernal Server Error");
+            } else {
+                response = {
+                    message: "File uploaded successfully",
+                    filename:req.file.originalname
+                };
+            }
+            console.log( response );
+            res.end( JSON.stringify( response ));
+        })
+    } )
+})
+
+
 
 var server = app.listen(8081, () => {
     var host = server.address().address;
